a7b4f23526d0559251b000f56e5d67ca80244628
kk210001 <kk210001@naver.com>
2022-06-22 20:02:56 +0900

    redirect는 실제 클라이언트에 응답이 나갔다가 redirect경로로 다시 요청한다 따라서 인지가능 forward는 서버 내부에서 일어나는 호출이기 때문에 클라이언트가 인지 못한다

---
+++ src/main/java/hello/servlet/domain/web/ServletMVC/MVCMemberFormServlet.java
@@ -0,0 +1,19 @@
+package hello.servlet.domain.web.ServletMVC;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name="mVCMemberFormServlet",urlPatterns = "/servlet-mvc/members/new-form")
+public class MVCMemberFormServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String viewPath = "/WEB-INF/views/new-form.jsp";
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewPath);
+        requestDispatcher.forward(request,response);//다른 서블릿, jsp 호출 서버 내부에서 다시 호출이 방생된다.
+    }
+}
