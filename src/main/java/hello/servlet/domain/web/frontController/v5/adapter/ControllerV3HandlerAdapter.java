0899b06cbf893f7845c6e5638aed620d8c01099c
kk210001 <kk210001@naver.com>
2022-06-27 21:08:37 +0900

    ControllerV5 ControllerV3와 ControllerV4를 동시에 쓰고 싶을때 쓰는 컨트롤러 컨트롤러를 handler로 생각하여 MyHandlerAdapter인터페이스 구현 뒤 어떠한 컨트롤러이닞 확인하는 boolean supports(Object handler)와 ModelView를 반환하는 handle를 생성 그리고 servlet에서는 핸들러를 담는 handlerAdapters와 핸들러의 매핑정보를 담는 handlerMappingMap를 생성하여 관리하는 것

---
+++ src/main/java/hello/servlet/domain/web/frontController/v5/adapter/ControllerV3HandlerAdapter.java
@@ -0,0 +1,37 @@
+package hello.servlet.domain.web.frontController.v5.adapter;
+
+import hello.servlet.domain.web.frontController.ModelView;
+import hello.servlet.domain.web.frontController.v3.ControllerV3;
+import hello.servlet.domain.web.frontController.v5.MyHandlerAdapter;
+import org.apache.tomcat.util.net.AbstractEndpoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
+    @Override
+    public boolean supports(Object handler) {
+        return (handler instanceof ControllerV3);
+    }
+
+    @Override
+    public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException {
+        ControllerV3 controller = (ControllerV3) handler;
+
+        Map<String, String> paramMap = createParamMap(request);
+        ModelView mv = controller.process(paramMap);
+
+        return mv;
+    }
+
+    private Map<String, String> createParamMap(HttpServletRequest request) {
+        Map<String, String> paramMap = new HashMap<>();
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
+        return paramMap;
+    }
+}
