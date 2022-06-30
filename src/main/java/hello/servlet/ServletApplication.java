f6d73bfed1bec2853f16f27b52ea18d3655d47bc
kk210001 <kk210001@naver.com>
2022-06-30 21:30:37 +0900

    @GetMapping, @PostMapping 각각 get, post방식으로 @RequestMapping을 해줌 @RequestParam http요청 파라미터를 받아줌 request.getParamger("")발전 버전
    느낀점 : 게시판을 책을 보고 혼자 볼 때 이해가 안갔던 RequestMapping의 기능이 뭔지와 컨트롤러에서 왜 String이나 ModelAndView를 반환해도 문제가 없는지 알게 되서 너무 기뻤다.

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -1,8 +1,12 @@
 package hello.servlet;
 
+import hello.servlet.domain.web.SpringMVC.v1.SpringMemberFormControllerV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan //서블릿 자동 등록
 @SpringBootApplication
@@ -11,5 +15,15 @@ public class ServletApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
+
+//	@Bean
+//	ViewResolver internalResourceViewResolver(){
+//		return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+//	} 스프링부트가 해주는 것
+
+//	@Bean
+//	SpringMemberFormControllerV1 springMemberFormControllerV1() {
+//		return new SpringMemberFormControllerV1();
+//	}
 
 }
